2e18cbf3816d8afe35cbe2386544b052f57c1d85
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-03-18 14:41:27 +0300

    linter configs was edited

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,10 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    extends: [
+        "eslint:recommended", 
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended"],
     overrides: [
         {
             env: {
